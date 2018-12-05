8336021845c51db36defa2d049d3b6b09bae311a
USAMAASGHAR <you@example.com>
2018-12-05 21:35:26 +0500

    final code

--- converter.cpp
+++ converter.cpp
@@ -15,8 +15,8 @@ public:
 
    double get_initial() {
        return initial_value;
-       }
 
+     }
    double get_converted() {
        return converted_value;
        }
@@ -40,15 +40,24 @@ class miles_kilos : public convert_base {
 
 public:
    miles_kilos(double passed_value) : convert_base(passed_value) { }
-
+   void convert_it(){
+     converted_value=initial_value/0.6;
+   }
 };
-class meters_yards{
-
-
+class meters_yards :public convert_base{
+public:
+  meters_yards(double passed_value) : convert_base(passed_value) { }
+    void convert_it(){
+    converted_value=initial_value*39.0/36.0;
+}
 };
-
-
-class Name:	yards_meters{
+class yards_meters : public convert_base{
+public:
+  yards_meters(double passed_value) : convert_base(passed_value) { }
+  void convert_it() {
+    converted_value=initial_value*36.0/39.0;
+  }
+};
 
 
 
@@ -57,40 +66,33 @@ class Name:	yards_meters{
 
 kilos_miles kilos1(100.0);
 kilos1.convert_it();
-
 cout << endl;
 cout << endl << "Distance in kilometers is: " << kilos1.get_initial();
 cout << endl << "Distance in miles is: " << kilos1.get_converted();
 cout << endl;
 
-/* ADD CODE HERE TO:
-	1. CREATE A MILES TO KILOS OBJECT, SPECIFYING AN INTIAL VALUE OF 60.
-	2. CALL THE VIRTUAL FUNCTION FOR THAT OBJECT
-	3. PRINT OUT THE INITIAL AND CONVERTED VALUES.
-NOTE: Refer to the preceding code for kilos to miles for an example.  */
+miles_kilos miles1(100.0);
+miles1.convert_it();
+cout << endl;
+cout << endl << "Distance in miles is: " << miles1.get_initial();
+cout << endl << "Distance in kilometers is: " << miles1.get_converted();
+cout << endl;
 
-/* Complete the coding for this step BEFORE YOU PROCEED FURTHER. */
-
-
-
-
-
-
-
-/* ADD CODE HERE TO:
-	1. USE THE YARDS TO METERS ROUTINE - SPECIFY AN INITIAL VALUE OF 4.
-	2. PRINT OUT THE INITIAL AND CONVERTED VALUES.           */
-
-/* Complete the coding for this step BEFORE YOU PROCEED FURTHER. */
+yards_meters yards(23);
+yards.convert_it();
+cout << endl;
+cout << endl << "Distance in yards is: " << yards.get_initial();
+cout << endl << "Distance in meters is: " << yards.get_converted();
+cout << endl;
 
 
+meters_yards meters(100.0);
+meters.convert_it();
+cout << endl;
+cout << endl << "Distance in meters is: " << meters.get_initial();
+cout << endl << "Distance in yards is: " << meters.get_converted();
+cout << endl;
 
 
-/*
-	3. USE THE METERS TO YARDS ROUTINE - SPECIFY AN INITIAL VALUE OF 4.
-	4. PRINT OUT THE INITIAL AND CONVERTED VALUES.
-   See previous code for examples.
-*/
-
-return(0);
-} // end main
+return 0;
+}
